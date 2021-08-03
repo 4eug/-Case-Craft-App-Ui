a851d66b45c30e0532afcc2aa18f5f8276e87fce
4eug <debraheug@gmail.com>
2021-08-03 01:44:49 +0100

    My Architecture

---
+++ lib/pages/SplashScreens.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SplashScreens extends StatefulWidget {
+  const SplashScreens({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreensState createState() => _SplashScreensState();
+}
+
+class _SplashScreensState extends State<SplashScreens> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
